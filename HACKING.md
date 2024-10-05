434304c8d762f810f3176f125464701e7f83de3b
Enorovan <enorovan@gmail.com>
2024-10-05 21:16:18 +0200

    C;HN Steam 1.1.0

---
+++ HACKING.md
@@ -0,0 +1,68 @@
+# Building C;C Patch
+
+**THIS PAGE IS ONLY OF INTEREST TO NERDS** and people who'd like to make their own patches for Chaos;Child (or some other SciADV titles) - and even then, you'll need a nerd to deal with this.
+
+This is a snapshot of our root project for the C;C patch, with some assets removed.
+
+**It's going to be painful** and these instructions may not be complete. As you can see, our build is kind of a mess, and *you'll need to extract assets from an already patched game installation*. This isn't the release we'd like to have made, but we hope it's better than nothing.
+
+**Clone this repository recursively.** Some of our internal dependencies are submodules.
+
+### Licensing and stuff
+
+- Most of our code is released under liberal open source licenses, check the repositories
+- Please do not pirate the game
+- **If you release a patch, take care to find and change all relevant text to not misrepresent us as the authors of your patch**. There is a lot of such text scattered throughout:
+    - `content/languagebarrier/patchdef.json` (please use your own AppData directory too)
+    - `installer/userdata`
+    - Hardcoded in the launcher source code at `launcher/realboot` (note, there's a technical support link pointing to our Discord, please don't keep it that way without asking us first)
+    - EXE metadata in `terribuild.ps1`
+    - We probably forgot something
+
+Third-party dependencies included in *this* repository:
+
+- `7zS2.sfx`: [Customised 7-Zip SFX module](https://github.com/CommitteeOfZero/lzma-sdk) (public domain)
+- `rcedit-x86.exe`: [rcedit](https://github.com/electron/rcedit) (MIT)
+- `xdelta3.exe`: [xdelta3](https://github.com/jmacd/xdelta) version 3.1.1 (Apache 2.0)
+- `Newtonsoft.Json.dll`: [Json.NET](https://github.com/JamesNK/Newtonsoft.Json) (MIT)
+
+This doesn't fit anywhere else:
+
+- We made [REEEEEEGen](https://github.com/CommitteeOfZero/REEEEEEGen) (MIT) to generate some images.
+
+### External dependencies
+
+- Visual Studio 2015
+- Maybe Visual Studio 2017? Not sure if we need it for anything in here
+- Qt 5.9+ for Visual Studio 2015
+- Libraries for noidget (see `installer/noidget/conf.pri.sample`)
+- DirectX 9 SDK - make sure the `DXSDK_DIR` environment variable is set to e.g. `C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\`
+- Python 2.7 - make sure it's in PATH
+- [LanguageBarrier](https://github.com/CommitteeOfZero/LanguageBarrier/tree/multi-game) on branch `multi-game`
+- 7-Zip - make sure it's in PATH
+
+### Building dependencies
+
+- Build `SciAdv.Net` using the `build.cmd` in its subfolder (this also happens during the main build but you'll need it for asset extraction first).
+- You do not need to build LanguageBarrier or xdelta3, these are built during the main build and noidget build, respectively.
+
+### Preparing assets
+
+- Export edited images to the `cc-edited-images` subfolder in this repository. Sorry, no convenient way to do this right now - take `languagebarrier/c0data.mpk` from a patched copy and copy things to the appropriate paths according to `c0data_toc.csv`.
+- Build SciAdv.Net with its `build.cmd`.
+- Extract `languagebarrier/enscript.mpk` and `languagebarrier/enscript_c.mpk` with `ungelify` from SciAdv.Net somewhere.
+- Dump patched script text with `sc3tools` (also from SciAdv.Net): `sc3tools.exe extract-text *.scx cc` in the extraction folders.
+- Copy the output .txt files from `enscript.mpk` into a `cc-scripts` subfolder and the output .txt files from `enscript_c.mpk` into a `cc-scripts-consistency` subfolder.
+- Extract the Steam version's original `USRDIR/script.mpk` into the `script_archive_steam` subfolder.
+- Copy `languagebarrier/audio`, `languagebarrier/subs` and `languagebarrier/video` to `content/languagebarrier`
+
+### Configuration
+
+- Copy `installer/noidget/conf.pri.sample` to `installer/noidget/conf.pri` and adjust for your system.
+- Copy `config.ps1.sample` to `config.ps1` and adjust for your system
+
+### Actually doing the build
+
+- Run `terribuild.ps1`
+- Get a billion errors
+- [Cry](https://discord.gg/rq4GGCh)
---
+++ terribuild.ps1
@@ -0,0 +1,200 @@
+﻿[Reflection.Assembly]::LoadFrom("$((Get-Location).Path)\Newtonsoft.Json.dll")
+
+# Config
+
+try {
+    . ".\config.ps1"
+}
+catch {
+    throw "Please put a config.ps1 from the provided config.ps1.sample in the repository root, and run this script from there."
+}
+
+# EXE metadata configuration
+$version_string = "1.1.0"
+$tool_icon = "CoZIcon.ico"
+$game_icon = "LauncherIcon.ico"
+$publisher = "Committee of Zero"
+$product_name = "CHAOS;HEAD NOAH Overhaul Patch (Steam)"
+
+# Code
+
+function SetInstallerExeMetadata {
+    param ([string]$exePath)
+    $originalFilename = (Get-Item $exePath).Name
+    .\rcedit-x86.exe $exePath `
+        --set-icon "$tool_icon" `
+        --set-file-version "$version_string" `
+        --set-product-version "$version_string" `
+        --set-version-string "CompanyName" "$publisher" `
+        --set-version-string "FileDescription" "$product_name Installer (v$version_string)" `
+        --set-version-string "FileVersion" "$version_string" `
+        --set-version-string "InternalName" "Installer.exe" `
+        --set-version-string "LegalCopyright" "$publisher" `
+        --set-version-string "OriginalFilename" "$originalFilename" `
+        --set-version-string "ProductName" "$product_name Installer" `
+        --set-version-string "ProductVersion" "$version_string"
+}
+function SetUninstallerExeMetadata {
+    param ([string]$exePath)
+    $originalFilename = (Get-Item $exePath).Name
+    .\rcedit-x86.exe $exePath `
+        --set-icon "$tool_icon" `
+        --set-file-version "$version_string" `
+        --set-product-version "$version_string" `
+        --set-version-string "CompanyName" "$publisher" `
+        --set-version-string "FileDescription" "$product_name Uninstaller (v$version_string)" `
+        --set-version-string "FileVersion" "$version_string" `
+        --set-version-string "InternalName" "nguninstall.exe" `
+        --set-version-string "LegalCopyright" "$publisher" `
+        --set-version-string "OriginalFilename" "$originalFilename" `
+        --set-version-string "ProductName" "$product_name Uninstaller" `
+        --set-version-string "ProductVersion" "$version_string"
+}
+
+function SetRealbootExeMetadata {
+    param ([string]$exePath)
+    $originalFilename = (Get-Item $exePath).Name
+    .\rcedit-x86.exe $exePath `
+        --set-icon "$game_icon" `
+        --set-file-version "$version_string" `
+        --set-product-version "$version_string" `
+        --set-version-string "CompanyName" "$publisher" `
+        --set-version-string "FileDescription" "$product_name Launcher (v$version_string)" `
+        --set-version-string "FileVersion" "$version_string" `
+        --set-version-string "InternalName" "realboot.exe" `
+        --set-version-string "LegalCopyright" "$publisher" `
+        --set-version-string "OriginalFilename" "$originalFilename" `
+        --set-version-string "ProductName" "$product_name Launcher" `
+        --set-version-string "ProductVersion" "$version_string"
+}
+
+function GenerateEnscriptToc {
+    param ([string]$tocPath, [string]$scriptsPath)
+    $inToc = Import-CSV .\script_toc.csv -header Id, FilenameOnDisk, FilenameInArchive
+    $jw = New-Object Newtonsoft.Json.JsonTextWriter(New-Object System.IO.StreamWriter($tocPath))
+    $jw.Formatting = [Newtonsoft.Json.Formatting]::Indented
+    $jw.Indentation = 2
+    $jw.IndentChar = ' '
+    $jw.WriteStartArray();
+    foreach ($entry in $inToc) {
+        $jw.WriteStartObject();
+        $jw.WritePropertyName("id");
+        $jw.WriteValue([int]$entry.Id);
+        $jw.WritePropertyName("filename");
+        $jw.WriteValue($entry.FilenameInArchive);
+        $jw.WritePropertyName('size');
+        $jw.WriteValue((Get-Item "$scriptsPath\$($entry.FilenameInArchive)").Length);
+        $jw.WriteEndObject();
+    }
+    $jw.WriteEndArray();
+    $jw.Flush()
+    $jw.Close()
+}
+
+# END CONFIG
+
+function PrintSection {
+    param ([string]$desc)
+    $line = "------------------------------------------------------------------------"
+    $len = (($line.length, $desc.legnth) | Measure -Max).Maximum
+    
+    Write-Host ""
+    Write-Host $line.PadRight($len) -BackgroundColor DarkBlue -ForegroundColor Cyan
+    Write-Host ("      >> " + $desc).PadRight($len) -BackgroundColor DarkBlue -ForegroundColor Cyan
+    Write-Host $line.PadRight($len) -BackgroundColor DarkBlue -ForegroundColor Cyan
+    Write-Host ""
+}
+
+Write-Output "                          ＴＥＲＲＩＢＵＩＬＤ"
+Write-Output "Rated World's #1 Build Script By Leading Game Industry Officials"
+Write-Output ""
+Write-Output "------------------------------------------------------------------------"
+Write-Output ""
+
+PrintSection "Creating new DIST and temp"
+Remove-Item -Force -Recurse -ErrorAction SilentlyContinue .\DIST
+New-Item -ItemType directory -Path .\DIST | Out-Null
+Remove-Item -Force -Recurse -ErrorAction SilentlyContinue .\temp
+New-Item -ItemType directory -Path .\temp | Out-Null
+Remove-Item -Force -Recurse -ErrorAction SilentlyContinue .\symbols
+New-Item -ItemType directory -Path .\symbols | Out-Null
+
+PrintSection "Pulling latest script changes"
+cd coalesc3
+& git pull
+cd data/chaos_head/txt_eng
+& git pull
+cd ../../../..
+
+PrintSection "Building LanguageBarrier as $languagebarrier_configuration|$languagebarrier_platform"
+& "$msbuild" "$languagebarrier_dir\LanguageBarrier.sln" "/p:Configuration=$languagebarrier_configuration" "/p:Platform=$languagebarrier_platform"
+
+PrintSection "Copying LanguageBarrier to DIST"
+Copy-Item $languagebarrier_dir\x64\$languagebarrier_configuration\*.dll .\DIST
+Copy-Item $languagebarrier_dir\x64\$languagebarrier_configuration\*.pdb .\symbols
+Copy-Item -Recurse $languagebarrier_dir\x64\$languagebarrier_configuration\languagebarrier .\DIST
+New-Item -ItemType directory -Path ".\DIST\HEAD NOAH" | Out-Null
+# TODO how does wine handle this?
+Move-Item .\DIST\*.dll ".\DIST\HEAD NOAH\"
+# Reported necessary for some users, otherwise:
+# "Procedure entry point csri_renderer_default could not be located in ...\HEAD NOAH\DINPUT8.dll"
+Copy-Item ".\DIST\HEAD NOAH\VSFilter.dll " .\DIST\
+
+PrintSection "Patching scripts"
+cd coalesc3
+python build.py chaos_head windows all --clean
+Copy-Item .\out\chaos_head\windows\*.cpk ..\DIST\languagebarrier\
+cd ..
+
+PrintSection "Packing c0data.cpk"
+cd .\cri-tools
+python .\create_archive.py --directory ..\c0data\ --archive ..\DIST\languagebarrier\c0data.cpk
+cd ..
+
+# LanguageBarrier currently needs this file to be present even if no string redirections are configured
+echo $null > .\content\languagebarrier\stringReplacementTable.bin
+
+PrintSection "Copying content to DIST"
+Copy-Item -Recurse -Force .\content\* .\DIST
+
+PrintSection "Building and copying realboot"
+cd launcher
+& .\realboot_build.bat
+cd ..
+SetRealbootExeMetadata .\launcher\deploy\Game_Steam.exe
+Copy-Item -Recurse -Force .\launcher\deploy\* .\DIST
+Copy-Item -Recurse -Force .\launcher\build\release\*.pdb .\symbols
+
+PrintSection "Building noidget"
+cd installer
+& .\noidget_build.bat
+cd ..
+SetInstallerExeMetadata .\installer\deploy\noidget.exe
+SetUninstallerExeMetadata .\installer\deployUninstaller\noidget.exe
+Copy-Item -Recurse -Force .\installer\build\release\*.pdb .\symbols
+
+PrintSection "Packing uninstaller"
+cd installer\deployUninstaller
+7z a -mx=0 ..\..\temp\sfxbaseUninstaller.7z .\*
+cd ..\..
+copy .\7zS2.sfx .\temp\UninstallerExtractor.exe
+SetUninstallerExeMetadata -exePath .\temp\UninstallerExtractor.exe
+cmd /c copy /b .\temp\UninstallerExtractor.exe + .\temp\sfxbaseUninstaller.7z DIST\nguninstall.exe
+
+PrintSection "Packing installer"
+cd temp
+$patchFolderName = "CHNSteamPatch-v$version_string-Setup"
+New-Item -ItemType directory -Path $patchFolderName | Out-Null
+cd $patchFolderName
+New-Item -ItemType directory -Path DIST | Out-Null
+Move-Item -Force ..\..\DIST\* .\DIST
+New-Item -ItemType directory -Path STEAMGRID | Out-Null
+Copy-Item -Recurse -Force  ..\..\content_steamgrid\* .\STEAMGRID
+Move-Item -Force ..\..\installer\deploy\* .
+Move-Item -Force .\noidget.exe .\CHNSteamPatch-Installer.exe
+cd ..\..\DIST
+7z a -t7z -mx=5 -m0=lzma2 "$patchFolderName.zip" "..\temp\$patchFolderName"
+cd ..
+
+PrintSection "Removing temp"
+Remove-Item -Force -Recurse .\temp
