551a191a01a5ed4c066e524a31dd2a729ff6c683
Enorovan <enorovan@gmail.com>
2025-06-07 00:17:22 +0200

    C;HN GOG 1.1.0

--- launcher/realboot_build.bat
+++ launcher/realboot_build.bat
@@ -2,14 +2,14 @@
 pushd build
 call vcvarsall.bat x86
 set CL=/FC %CL%
-qmake "CONFIG+=chaosheadnoah" "CONFIG+=steam" ..\realboot
+qmake "CONFIG+=chaosheadnoah" ..\realboot
 nmake
 popd
 copy /y realboot\vendor\curl\bin\libcurl.pdb build\release\
 if exist deploy rmdir /q /s deploy
 mkdir deploy
 pushd deploy
-copy ..\build\release\realboot.exe .\Game_Steam.exe
+copy ..\build\release\realboot.exe .\Game.exe
 copy /y ..\realboot\vendor\curl\bin\libcurl.dll .\
-windeployqt --no-translations --no-compiler-runtime --no-quick-import --no-system-d3d-compiler --no-webkit2 --no-angle --no-opengl-sw .\Game_Steam.exe
+windeployqt --no-translations --no-compiler-runtime --no-quick-import --no-system-d3d-compiler --no-webkit2 --no-angle --no-opengl-sw .\Game.exe
 popd
